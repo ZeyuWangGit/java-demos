9c2e1b5c46f9d23afb648212d39b5d3023e49586
Zeyu Wang <wzy.franklin@gmail.com>
2025-05-09 15:56:58 +1000

    update

--- big-event/src/main/java/com/exp/controller/UserController.java
+++ big-event/src/main/java/com/exp/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.exp.controller;
 
+import com.exp.dto.UserRegisterDTO;
 import com.exp.pojo.Result;
 import com.exp.pojo.User;
 import com.exp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +18,12 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
-        User user = userService.findByUsername(username);
+    public Result register(@RequestBody UserRegisterDTO dto) {
+        User user = userService.findByUsername(dto.getUsername());
         if (user != null) {
             return Result.error("User already exists");
         } else {
-            userService.register(username, password);
+            userService.register(dto.getUsername(), dto.getPassword());
             return Result.success();
         }
 
---
+++ big-event/src/main/java/com/exp/dto/UserRegisterDTO.java
@@ -0,0 +1,9 @@
+package com.exp.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegisterDTO {
+    private String username;
+    private String password;
+}
