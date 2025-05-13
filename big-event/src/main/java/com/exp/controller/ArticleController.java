76a5a339ea02d2f2138fffa4f922d5e87733a542
ZeyuWangHipages <zeyuwang@hipagesgroup.com.au>
2025-05-13 12:10:08 +1000

    Update ArticleController.java

--- big-event/src/main/java/com/exp/controller/ArticleController.java
+++ big-event/src/main/java/com/exp/controller/ArticleController.java
@@ -1,10 +1,7 @@
 package com.exp.controller;
 
 import com.exp.pojo.Result;
-import com.exp.utils.JwtUtil;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
