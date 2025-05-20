4ad630c0f9f0d759e392431fdd71813e9072277c
Zeyu Wang <wzy.franklin@gmail.com>
2025-05-20 14:53:55 +1000

    feat: add source code analysis template

---
+++ code-source/src/main/java/com/exp/codesource/CodeSourceApplication.java
@@ -0,0 +1,13 @@
+package com.exp.codesource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSourceApplication.class, args);
+    }
+
+}
---
+++ code-source/src/main/java/com/exp/codesource/initializer/MyApplicationContextInitializer.java
@@ -0,0 +1,4 @@
+package com.exp.codesource.initializer;
+
+public class MyApplicationContextInitializer {
+}
